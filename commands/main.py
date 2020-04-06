07103508c48b6c4bc0e546b32652b4b173b6f488
Sokhibjon Orzikulov <sakhib.orzklv@gmail.com>
2020-04-06 23:03:04 +0500

    Created auto restart function to make bot restart itself (●'◡'●)

--- commands/main.py
+++ commands/main.py
@@ -1,14 +1,13 @@
-try:
-    from commands.start import cmd_start
-    from commands.help import cmd_help
-    from commands.error import cmd_error
-except ImportError as err:
-    print(f"Import failed, {err}")
-    exit(1)
+from commands.start import cmd_start
+from commands.help import cmd_help
+from commands.error import cmd_error
+from commands.restart import cmd_restart
 
 
 def commands():
     cmd_start()
+    cmd_restart()
     cmd_help()
     cmd_error()
+
     pass
